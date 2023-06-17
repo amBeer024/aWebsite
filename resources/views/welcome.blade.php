2ef0d504e4e66314291bc239ac3efabc8f1162a4
amBeer024 <ambeer024@gmail.com>
2023-06-17 15:29:36 +0200

    opmaak verbeteren

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,23 @@
             {{ __('Reizen') }}
         </h2>
     </x-slot>
-    {{ __() }}
-    <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-        @foreach ($vacations as $vacation)
-        <div class="p-6 flex space-x-2">
-           
-            <div class="flex-1">
-                <div class="flex justify-between items-center">
-                    <div>
-                        {{-- {{ dump($vacation) }} --}}
-                        <span class="text-gray-800">{{ $vacation->city->city_name }}</span>
-                        <span class="text-gray-800">{{ $vacation->city->country->country_name }}</span>
-                        <span class="text-gray-800">{{ $vacation->description}}</span>
-                        {{-- <small class="ml-2 text-sm text-gray-600">{{ $vacation->created_at->format('j M Y, g:i a')
-                            }}</small> --}}
-                    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @foreach ($vacations as $vacation)
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <span class="text-gray-800">{{ $vacation->city->city_name }}</span>
+                    <span class="text-gray-800">{{ $vacation->city->country->country_name }}</span>
+                    <span class="text-gray-800">{{ $vacation->description}}</span>
                 </div>
-                {{-- <p class="mt-4 text-lg text-gray-900">{{ $vacation->id }}</p> --}}
             </div>
+
+
+
+            @endforeach
         </div>
-        @endforeach
     </div>
+
+
 </x-app-layout>
